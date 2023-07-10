96d0426ba5c2275e46c8911ecc754579b6fe0bcb
Mohammad <mohammad76pakravan@gmail.com>
2023-07-10 17:29:10 +0430

    darse if and else

---
+++ src/main/kotlin/darse alichiiiii.kt
@@ -0,0 +1,31 @@
+fun main() {
+
+// if in if
+val num = readln().toInt()
+
+if (num >= 0 ){
+    if ((num % 2) == 0){
+        println("zooje mosbat")
+    }
+}
+
+// if and else if and else
+val num2 = readln().toInt()
+
+if ((num2 % 2) == 0){    println("bakhsh pazire bw 2")
+}
+else if ((num2 % 3) == 0){
+    println("bakhsh pazire bw 3")
+}else if ((num2 % 5) == 0){
+    println("bakhsh pazire bw 5")
+}else if ((num2 % 7) == 0){
+    println("bakhsh pazire bw 7")
+}
+else{
+    println("velemoon kon")
+}
+
+
+//    num == 2 -> true
+
+}
---
+++ src/main/kotlin/darse6.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val num = readln().toInt()
+
+    if (num == 2) {
+        println("your numbe is 2")
+    }
+    if (num > 0) {
+        println("adad shoma mosbat")
+//
+
+    }
+
+
+
+}
+
